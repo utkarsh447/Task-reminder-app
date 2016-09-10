09f77815aab4b7b577ec603f66dfb796ebbf2373
Utkarsh Anand <ua.9810@gmail.com>
2016-09-11 01:51:42 +0530

    The first start

---
+++ app/src/main/java/com/reminder/utkarsh/tasksnew1/OnAlarmReceiver.java
@@ -0,0 +1,27 @@
+package com.reminder.utkarsh.tasksnew1;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ComponentInfo;
+import android.util.Log;
+
+public class OnAlarmReceiver extends BroadcastReceiver {
+
+    private static final String TAG = ComponentInfo.class.getCanonicalName();
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "Received wake up from alarm manager.");
+
+        long rowid = intent.getExtras().getLong(RemindersDbAdapter.KEY_ROWID);
+
+        WakeReminderIntentService.acquireStaticLock(context);
+
+        Intent i = new Intent(context, ReminderService.class);
+        i.putExtra(RemindersDbAdapter.KEY_ROWID, rowid);
+        context.startService(i);
+
+    }
+}
